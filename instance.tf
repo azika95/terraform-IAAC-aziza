0249351f4cf1d126b7a7d5b28f017c5917b7abb5
azizasalieva95 <azika1695@gmail.com>
2019-12-16 20:35:46 -0600

    edited

--- instance.tf
+++ instance.tf
@@ -4,9 +4,9 @@ resource "aws_instance" "web" {
  
 
  associate_public_ip_address = "true"
- key_name = "aws_key_pair.deployer.key_name" 
+ key_name = aws_key_pair.deployer.key_name 
 
  tags = { 
     Name = "HelloWorld" 
   } 
-} 
+}
