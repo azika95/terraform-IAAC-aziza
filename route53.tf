7194406cea022a3028a13b1b8d3edfde537681b8
azizasalieva95 <azika1695@gmail.com>
2019-12-18 19:21:40 -0600

    added

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" { 
+  zone_id = "Z3RXR67RBGOOLJ" 
+  name    = "www.example.com" 
+  type    = "A" 
+  ttl     = "60" 
+  records = [aws_instance.web.public_ip] 
+}
