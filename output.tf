d059310e1bc5d0e7330a03e22a6c79611044eec1
azizasalieva95 <azika1695@gmail.com>
2019-12-18 19:43:49 -0600

    edited

--- output.tf
+++ output.tf
@@ -1,15 +1,22 @@
 output "bucket_name" {
-    value = aws_s3_bucket.b.bucket
+    value = aws_s3_bucket.b.bucket 
 }
 output "sec_group" {
-    value = aws_security_group.allow.tls.name
-}
-output "key_name" {
+    value = aws_security_group.allow_tls.name
+     }
+ output "key_name" {
     value = aws_key_pair.deployer.key_name
 }
 output "username" {
-    "some_username"
+    value = "some_username" 
 }
 output "password" {
-    value "password"
+    value = "password"
 }
+output "wordpress_username" {
+    value = "some_username"
+}
+output "wordpress_paswword" {
+    value = "password"
+}
+
