8d387380475389d5db6b4ad5c3e657b644026bbe
azizasalieva95 <azika1695@gmail.com>
2019-12-21 12:35:55 -0600

    fixed

--- instance.tf
+++ instance.tf
@@ -2,23 +2,22 @@ resource "aws_instance" "web" {
   count = var.count_instance
   ami             = var.ami
   instance_type   = var.instance_type
-  associate_public_ip_address = "true"
+  associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_tls"]
-  provisioner "remote-exec" { 
-    connection { 
-      host        = self.public_ip
-      type        = "ssh"
-      user        = var.user
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = var.user
       private_key = file(var.ssh_key_location)
-      } 
-      inline = [ 
+      }
+      inline = [
         "sudo yum install -y epel-release",
-        "sudo yum install httpd -y",
-        "systemctl start httpd" ,
-        ] 
-        } 
-  
+        "sudo yum install httpd -y ",
+        "systemctl start httpd",
+        ]
+      }
   lifecycle{
     prevent_destroy = false
   }
