702b6d5645386834dcc7640fe7e324aba76c6aad
azizasalieva95 <azika1695@gmail.com>
2019-12-21 11:29:15 -0600

    editd

--- instance.tf
+++ instance.tf
@@ -1,16 +1,21 @@
-resource "aws_instance" "web" { 
-  ami           = "ami-00068cd7555f543d5" 
-  instance_type = "t2.micro" 
- 
-
- associate_public_ip_address = "true"
- key_name = aws_key_pair.deployer.key_name 
-
- user_data = file("userdata_file")
- security_groups = ["allow_tls"]
-
-
- tags = { 
-    Name = "HelloWorld"
-  } 
+resource "aws_instance" "web" {
+  count = 10
+  ami             = var.ami
+  instance_type   = "t2.micro"
+  associate_public_ip_address = "true"
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = ["allow_ssh"]
+  user_data = file("userdata_file")
+  lifecycle{
+    prevent_destroy = false
+  }
+  tags = {
+    Name = "HelloWorld${count.index +1}"
+  }
+}
+resource "aws_instance" "imported" {
+  ami             = "ami-00068cd7555f543d5"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = ["allow_ssh"]
+  instance_type  = "t2.micro"
 }
