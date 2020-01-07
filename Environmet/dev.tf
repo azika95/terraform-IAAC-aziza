bfbb012350dd21f2eb74ff82dc4eb74053530d4a
azizasalieva95 <azika1695@gmail.com>
2020-01-06 20:31:29 -0600

    added

--- Environmet/dev.tf
+++ Environmet/dev.tf
@@ -1,8 +1,5 @@
-name = "wordpres1"
-cidr = "10.0.0.0/16"
-
 environment = "dev"
-s3_bucket = "wordpress-farrukh-2019-dev"   #Will be used to set backend.tf
+s3_bucket = "wordpress-aziza-2019-dev"   #Will be used to set backend.tf
 s3_folder_project = "application"          #Will be used to set backend.tf
 s3_folder_region = "us-east-1"             #Will be used to set backend.tf
 s3_folder_type = "state"                   #Will be used to set backend.tf
