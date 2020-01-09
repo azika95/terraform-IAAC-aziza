ee08e5a64b81ca6c5686c62debc46ee1926857da
azizasalieva95 <azika1695@gmail.com>
2020-01-08 19:15:07 -0600

    changed

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -16,10 +16,11 @@ output "ami" {
     value = "${data.aws_ami.ubuntu.id}" 
     }
 
-    resource "aws_instance" "web" {
+resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
     }
+}
