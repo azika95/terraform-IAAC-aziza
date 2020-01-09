6e4fee74fe88a10dac18b93ba0e5821a7467cedb
azizasalieva95 <azika1695@gmail.com>
2020-01-08 18:51:23 -0600

    edited

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -11,3 +11,12 @@ provider "aws" {
 output "ami" {
     value = "${data.aws_ami.ubuntu.id}" 
     }
+
+    resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
