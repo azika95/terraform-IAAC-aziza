9e06e167c7f2b446ac75a917e3383b9fe979318b
azizasalieva95 <azika1695@gmail.com>
2020-01-18 12:49:41 -0600

    added

--- Azure/provider.tf
+++ Azure/provider.tf
@@ -167,9 +167,126 @@ resource "azurerm_virtual_machine" "vm1" {
 } 
 
   tags { 
-      
+
     environment = "staging" 
 
   } 
 
 }
+
+
+
+
+
+# VM 2 
+
+resource "azurerm_network_interface" "nic2" { 
+
+  name = "nic2" 
+
+  location = "westus2" 
+
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+
+  ip_configuration { 
+
+    name = "testconfiguration2" 
+
+    subnet_id = "${azurerm_subnet.private.id}" 
+
+    private_ip_address_allocation = "Dynamic" 
+
+    public_ip_address_id = "${azurerm_public_ip.IP2.id}" 
+
+  } 
+
+} 
+
+ 
+
+ 
+
+resource "azurerm_public_ip" "IP2" { 
+
+  name = "public_ip2" 
+
+  location = "westus2" 
+
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+
+  allocation_method = "Dynamic" 
+
+} 
+
+ 
+
+ 
+
+ 
+
+resource "azurerm_virtual_machine" "vm2" { 
+
+  name = "vm2" 
+
+  location = "westus2" 
+
+  resource_group_name = "${azurerm_resource_group.web_server_rg.name}" 
+
+  network_interface_ids = ["${azurerm_network_interface.nic2.id}"] 
+
+  vm_size = "Standard_DS1_v2" 
+
+  storage_image_reference { 
+
+  publisher = "OpenLogic" 
+
+  offer = "CentOS" 
+
+  sku = "7.5" 
+
+  version = "latest" 
+
+} 
+
+storage_os_disk { 
+
+  name = "myosdisk2" 
+
+  caching = "ReadWrite" 
+
+  create_option = "FromImage" 
+
+  managed_disk_type = "Standard_LRS" 
+
+} 
+
+os_profile { 
+
+  computer_name = "vm2" 
+
+  admin_username = "centos" 
+
+} 
+
+os_profile_linux_config { 
+
+  disable_password_authentication = true 
+
+  ssh_keys { 
+
+  path = "/home/centos/.ssh/authorized_keys" 
+
+  key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOUp9iibxgpJvfjCzUQOEQdNHiNqC30DD5s2SWmFqb/RogU0KFVS/y5CZrbwg8SVVZsQ0K3nHwfVitIhcw8nrY/gAYHwF7Bn3gEJ33QLk+TURP6vBaiJLw6JitRuCwyrFmWXNtL7Os19CqYwtmfsMjxWKYhtsPF9V0hh6pafQ6UmOyFMcxPIaPuNlJgDZnablUl8lV4GUdy01LHanPOAbza4EaiZSvmGWxFoaaL+7LjzEVqIuoPQb7T6JicrU8UFLGdIOiC7tBhSfDzrnaJkSMLpvp2hcgEVOEu+qRDlTDrqWifHxspD/GCjAUnH2/zIaO8BlS//XBDxhCiskLngu/ root@ip-172-31-43-7.eu-west-1.compute.internal"
+  } 
+
+} 
+
+tags { 
+
+   environment = "staging" 
+
+  } 
+
+} 
+
+
