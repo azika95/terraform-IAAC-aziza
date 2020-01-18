e60e4e20e80f5b1c07167cd2725221a277bc3c47
azizasalieva95 <azika1695@gmail.com>
2020-01-18 12:59:58 -0600

    added

--- Azure/backend.tf
+++ Azure/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "example1"
+    resource_group_name  = "example"
     storage_account_name = "exampleaziza"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
+    access_key           = ""
   }
 }
